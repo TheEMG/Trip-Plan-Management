1edae49d69d9f11d002176756aac1a35e165a188
CursedCactusJack <165201142+CursedCactusJack@users.noreply.github.com>
2024-03-30 10:52:26 -0500

    Test commit

--- trip_plan.sql
+++ trip_plan.sql
@@ -1,1 +1,16 @@
-/* Cactus is a delicacy */
+/* Test DB*/
+DROP SCHEMA IF EXISTS Tester;
+CREATE SCHEMA Tester;
+USE Tester;
+
+CREATE TABLE Member(
+    Member_ID VARCHAR(30);
+
+    PRIMARY KEY Member_ID;
+)
+
+ALTER TABLE Member
+    ADD CONSTRAINT ID_Format
+        CHECK (Member_ID RLIKE "[:alpha:]{0,15}[0-9]{0,15}");
+
+DROP TABLE Member;
