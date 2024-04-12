049451779e2bdfe6e3118db646393e025654ea59
Eric Oliver <erico49867@gmail.com>
2024-04-12 01:18:50 -0500

    Added Values to SIGHTS Table

--- trip_plan.sql
+++ trip_plan.sql
@@ -490,9 +490,6 @@ INSERT INTO SHOPPING_MALLS VALUES
   (14, 14, 'Imagelink0014' ),
   (15, 15, 'Imagelink0015' );
  
-INSERT INTO RATE VALUES
--- (PLAN_ID = INT, MEMBER_ID = INT, RATING)
-()
 
 
 INSERT INTO TRIP_PLAN VALUES
@@ -538,13 +535,21 @@ VALUES
 (4, 8, 8, 'Cheddars', 'Experience delicious American cuisine in a cozy and inviting atmosphere.', '123 Oak Street, Muy Mal, Sonora', 4, '11:00 AM', '890-123-4567', 'American', '$$', 'www.cheddars.com'),
 (5, 9, 9, 'Starlight Diner', 'Step back in time and enjoy classic diner fare at this nostalgic eatery.', '456 Elm Avenue, Miami, Florida', 3, '6:00 AM', '901-234-5678', 'Diner', '$', 'www.starlightdiner.com');
 
--- COMMENT (Comment_ID, Destination_ID, Member_ID, Dislikes, Likes, Content_Rating, Comment_Date, Posting_Time)
+INSERT INTO SIGHTS VALUES
+-- (SIGHT_ID INT, ATTRACTION_ID = INT, CITY_ID = INT, Ticket_Price = $)
+(1, 4, 4, 26.69),
+(2, 7, 7, 32.69),
+(3, 11, 1, 4.2069),
+(4, 13, 3, 80.085),
+(5, 14, 4, 26.50);
 INSERT INTO COMMENTS VALUES
   (1, 1, 1, 5, 3, 2, '2004-06-09', '10:00:00'),
   (2, 2, 2, 10, 7, 3, '2019-04-20', '02:00:00'),
   (3, 3, 3, 15, 11, 4, '2020-09-01', '15:15:15'),
   (4, 4, 4, 20, 15, 5, '2021-10-02', '12:37:45'),
-  (5, 5, 5, 25, 19, 6, '2022-11-11', '11:11:11');INSERT INTO USER_ACTION VALUES
+  (5, 5, 5, 25, 19, 6, '2022-11-11', '11:11:11');
+  
+  INSERT INTO USER_ACTION VALUES
 (1, 1, TRUE, FALSE, "USER REPLY"),
 (2, 2, FALSE, TRUE, "USER REPLY"),
 (3, 3, TRUE, TRUE, "USER REPLY"),
