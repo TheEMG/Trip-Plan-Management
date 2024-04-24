3df7347446ad47f25db2acfa023cd4ff6e894dcb
CursedCactusJack <165201142+CursedCactusJack@users.noreply.github.com>
2024-04-23 22:06:46 -0500

    Changed Associated_Members to Associated_Member for Consistency

---
+++ Test.sql
@@ -0,0 +1,12 @@
+USE Erics;
+
+/* Query 4. Retrieve the names and countries of all 
+ * preferred users who never visited the US. */
+
+/* Authorized_Member --> Associated_Members --> TRIP PLAN --> Planned attractions --> Travel Attraction --> City --> State --> Country
+*/
+SELECT * -- AuM.Member_ID, PA.Plan_ID, PA.Attraction_ID 
+FROM Authorized_Member AuM
+RIGHT JOIN Associated_Member AsM ON AuM.Member_ID = AsM.Member_ID
+INNER JOIN Planned_Attractions PA ON AsM.Plan_ID = PA.Plan_ID
+INNER JOIN Travel_Attractions TA ON PA.Attraction_ID = TA.Attraction_ID;
