ab4240ee3948e8c4ed7d7d23201ecb047f86f10d
Eric Gutierrez <124335726+TheEMG@users.noreply.github.com>
2024-04-27 09:27:27 -0500

    re-added emilios queries

--- queries.sql
+++ queries.sql
@@ -82,39 +82,38 @@ WHERE AM.Member_ID IN (
     WHERE C.Comment_Date BETWEEN '2018-12-01' AND '2019-01-31' -- Checking if the date of the comment is within the date range (Only one time since we only have one date)
 );
 
+/* Query 4. Retrieve the names and countries of all 
+ * preferred users who never visited the US. */
+SELECT AuM.FName, AuM.LName
+FROM Authorized_Member AuM
+RIGHT JOIN Associated_Member AsM ON AuM.Member_ID = AsM.Member_ID
+INNER JOIN Planned_Attractions PA ON AsM.Plan_ID = PA.Plan_ID
+INNER JOIN Travel_Attractions TA ON PA.Attraction_ID = TA.Attraction_ID
+INNER JOIN City C ON TA.City_ID = C.City_ID
+INNER JOIN State S ON C.State_ID = S.State_ID
+INNER JOIN Country Co ON S.Country_Name = Co.Country_Name
+WHERE Co.Country_Name NOT LIKE "USA"
+GROUP BY AuM.Member_ID;
 
-SELECT 
-    CITY.City_Name,  -- The name of the city from the CITY table
-    COUNT(DISTINCT TRIP_PLAN.Plan_ID) AS NumberOfTrips  -- Counts unique trip plans per city
-FROM 
-    TRIP_PLAN  -- The table that contains details about each trip plan
-JOIN 
-    PLANNED_ATTRACTIONS ON TRIP_PLAN.Plan_ID = PLANNED_ATTRACTIONS.Plan_ID  -- Linking trip plans to attractions
-JOIN 
-    TRAVEL_ATTRACTIONS ON PLANNED_ATTRACTIONS.Attraction_ID = TRAVEL_ATTRACTIONS.Attraction_ID  -- Connects planned attractions to actual attractions
-JOIN 
-    CITY ON TRAVEL_ATTRACTIONS.City_ID = CITY.City_ID  -- Joins attractions to their cities
-JOIN 
-    STATE ON CITY.State_ID = STATE.State_ID  -- Joins cities to their states to filter by country
-WHERE 
-    STATE.Country_Name = 'France'  -- Filters cities to only include those in France
-GROUP BY 
-    CITY.City_Name  -- Groups results by city name
-ORDER BY 
-    NumberOfTrips DESC  -- Order cities by the number of trip plans, descending
-LIMIT 5;  -- Limits results to the top 5 cities
-
--- Query 8. Retrieve the username and status of the member who either posted any comments or created any itinerary between 12/01/2018 and 1/31/2019.
-SELECT DISTINCT AM.User_Name, AM.Is_Preferred
-FROM AUTHORIZED_MEMBER AM
-WHERE AM.Member_ID IN (
-    -- Starting subquery to find member IDs from trip plans within specified date range
-    SELECT TP.Member_ID
-    FROM TRIP_PLAN TP
-    WHERE TP.Start_Date BETWEEN '2018-12-01' AND '2019-01-31' -- Checking if the start date of the trip is within the date range
-    OR TP.End_Date BETWEEN '2018-12-01' AND '2019-01-31' -- Checking again if but this time the end date??? I think this is necessary 
-    UNION -- Combinie member IDs from comments with the same date range
-    SELECT C.Member_ID
-    FROM COMMENTS C
-    WHERE C.Comment_Date BETWEEN '2018-12-01' AND '2019-01-31' -- Checking if the date of the comment is within the date range (Only one time since we only have one date)
-);
+/* Query 9. For each member who has visited all the
+ * states/provinces in France, retrieve the maximum, 
+ * minimum and average daily cost per person, per day 
+ * across all these related itineraries (considering 
+ * restaurant and attraction cost only). */
+SELECT AuM.FName, AuM.LName, MAX(person_daily_cost) AS max_daily_cost, MIN(person_daily_cost) AS min_daily_cost, AVG(person_daily_cost) AS avg_daily_cost
+FROM(
+    SELECT AsM.Member_ID, TP.Plan_ID, TP.Duration, (SUM(R.Price_range) + SUM(TP.Potential_Cost)) / TP.Duration AS person_daily_cost
+    FROM AUTHORIZED_MEMBER AuM
+    INNER JOIN ASSOCIATED_MEMBER AsM ON AuM.Member_ID = AsM.Member_ID
+    INNER JOIN TRIP_PLAN TP ON AsM.Plan_ID = TP.Plan_ID
+    INNER JOIN PLANNED_ATTRACTIONS PA ON TP.Plan_ID = PA.Plan_ID
+    INNER JOIN TRAVEL_ATTRACTIONS TA ON PA.Attraction_ID = TA.Attraction_ID
+    LEFT JOIN RESTAURANTS R ON TA.Attraction_ID = R.Attraction_ID
+    INNER JOIN CITY C ON TA.City_ID = C.City_ID
+    INNER JOIN STATE S ON C.State_ID = S.State_ID
+    INNER JOIN COUNTRY CO ON s.Country_Name = CO.Country_Name
+    WHERE CO.Country_Name = 'France'
+    GROUP BY AsM.Member_ID, TP.Plan_ID, TP.Duration
+)AS subquery
+INNER JOIN AUTHORIZED_MEMBER AuM ON subquery.Member_ID = AuM.Member_ID
+GROUP BY AuM.FName, AuM.LName;
