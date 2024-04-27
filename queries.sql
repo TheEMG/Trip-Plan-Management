c1bc58b28e1384f2f2d0cee9d942059ffade5347
CursedCactusJack <165201142+CursedCactusJack@users.noreply.github.com>
2024-04-27 11:45:37 -0500

    Formatted Queries for Consistency

--- queries.sql
+++ queries.sql
@@ -4,58 +4,36 @@ USE Erics;
 -- Query 1
 -- Retrieve the names, addresses, and estimated-prices of the 3 most popular restaurants in New York city, US.
 -- I modified this to San Antonio, Texas instead of New York city, US.
-SELECT 
-    Restaurant_name,
-    Restaurant_address,
-    Price_range
-FROM 
-    RESTAURANTS AS R
-JOIN 
-    TRAVEL_ATTRACTIONS AS TA ON R.Attraction_ID = TA.Attraction_ID
-JOIN 
-    CITY AS C ON TA.City_ID = C.City_ID
-JOIN 
-    DESTINATION AS D ON TA.City_ID = D.Destination_ID
-WHERE 
-    D.Country_Name = 'USA' 
+SELECT Restaurant_name, Restaurant_address, Price_range
+FROM RESTAURANTS AS R
+JOIN TRAVEL_ATTRACTIONS AS TA ON R.Attraction_ID = TA.Attraction_ID
+JOIN CITY AS C ON TA.City_ID = C.City_ID
+JOIN DESTINATION AS D ON TA.City_ID = D.Destination_ID
+WHERE D.Country_Name = 'USA' 
     AND C.City_Name = 'San Antonio'
-ORDER BY 
-    R.Rating DESC
-LIMIT 3;
+ORDER BY R.Rating DESC LIMIT 3;
 
 -- Eric Oliver
 -- Query 2
 -- Retrieve the username, status (regular or preferred), and ranking of the member who has uploaded the most pictures.
-SELECT 
-    U.User_Name,
-    U.Is_Preferred,
-    U.Ranking
-FROM 
-    AUTHORIZED_MEMBER AS U
-JOIN 
-    IMAGES AS I ON U.Member_ID = I.Member_ID
-GROUP BY 
-    U.Member_ID
-ORDER BY 
-    COUNT(I.Image_ID) DESC
-LIMIT 1;
--- Zachary's Queries :D
+SELECT U.User_Name, U.Is_Preferred, U.Ranking
+FROM AUTHORIZED_MEMBER AS U
+JOIN IMAGES AS I ON U.Member_ID = I.Member_ID
+GROUP BY U.Member_ID
+ORDER BY COUNT(I.Image_ID) DESC LIMIT 1;
 
+
+
+-- Zachary's Queries :D
 /* Query 5: For all country(s) visited by those who also visited the US 
 on the same trip (within 15 days), retrieve the distinct names of the countries. */
-
 SELECT DISTINCT S1.Country_Name
-FROM 
-    PLANNED_ATTRACTIONS P1 -- Gets PLANNED_ATTRACTIONS
-JOIN 
-    TRAVEL_ATTRACTIONS TA1 ON P1.Attraction_ID = TA1.Attraction_ID  -- Joins PLANNED_ATTRACTIONS and TRAVEL_ATTRACIONS
-JOIN 
-    CITY C1 ON TA1.City_ID = C1.City_ID  -- Joins TRAVEL_ATTRACTIONS and CITY
-JOIN 
-    STATE S1 ON C1.State_ID = S1.State_ID  -- Joins CITY and STATE
+FROM PLANNED_ATTRACTIONS P1 -- Gets PLANNED_ATTRACTIONS
+JOIN TRAVEL_ATTRACTIONS TA1 ON P1.Attraction_ID = TA1.Attraction_ID  -- Joins PLANNED_ATTRACTIONS and TRAVEL_ATTRACIONS
+JOIN CITY C1 ON TA1.City_ID = C1.City_ID  -- Joins TRAVEL_ATTRACTIONS and CITY
+JOIN STATE S1 ON C1.State_ID = S1.State_ID  -- Joins CITY and STATE
 WHERE S1.Country_Name <> 'USA' AND EXISTS (SELECT * -- Makes sure S1 is not in the USA and compares to another date
-			  FROM 
-					PLANNED_ATTRACTIONS P2   -- GETS PLANNED_ATTRACTIONS
+			  FROM PLANNED_ATTRACTIONS P2   -- GETS PLANNED_ATTRACTIONS
 			 RIGHT JOIN 
 					TRAVEL_ATTRACTIONS TA2 ON P2.Attraction_ID = TA2.Attraction_ID  -- Connects PLANNED_ATTRACTIONS and TRAVEL_ATTRACTIONS
 			 RIGHT JOIN 
@@ -67,16 +45,14 @@ WHERE S1.Country_Name <> 'USA' AND EXISTS (SELECT * -- Makes sure S1 is not in t
 												   AND DATE_ADD(P1.Arrival_Date, INTERVAL 15 DAY) 
 							  OR P2.Departure_Date BETWEEN DATE_ADD(P1.Departure_Date, INTERVAL -15 DAY)
 												   AND DATE_ADD(P1.Departure_Date, INTERVAL 15 DAY)));
-                                                   
+
+-- Zachary's Queries :D
 /* Query 6: Retrieve the contact information of the business owner 
 who owns the most expensive restaurant and the owner who owns the most assets in the system.*/
-
 SELECT BUSINESS_OWNER.Contact_Info
 FROM BUSINESS_OWNER -- Gets BUSINESS_OWNER
-RIGHT JOIN 
-		TRAVEL_ATTRACTIONS ON BUSINESS_OWNER.Owner_ID = TRAVEL_ATTRACTIONS.Owner_ID -- Joins TRAVEL_ATTRACTIONS and BUSINESS_OWNER
-LEFT JOIN
-		RESTAURANTS ON TRAVEL_ATTRACTIONS.Attraction_ID = RESTAURANTS.Attraction_ID -- Joins RESTAURANTS and TRAVEL_ATTRACTIONS
+RIGHT JOIN TRAVEL_ATTRACTIONS ON BUSINESS_OWNER.Owner_ID = TRAVEL_ATTRACTIONS.Owner_ID -- Joins TRAVEL_ATTRACTIONS and BUSINESS_OWNER
+LEFT JOIN RESTAURANTS ON TRAVEL_ATTRACTIONS.Attraction_ID = RESTAURANTS.Attraction_ID -- Joins RESTAURANTS and TRAVEL_ATTRACTIONS
 WHERE RESTAURANTS.Price_range = (SELECT MAX(RESTAURANTS.Price_range) -- Gets the BUSINESS_OWNER with the most expensive RESTAURANT
 								 FROM RESTAURANTS)
 		OR TRAVEL_ATTRACTIONS.Owner_ID = (SELECT MAX(ASSETS.count_owners)
@@ -84,9 +60,11 @@ WHERE RESTAURANTS.Price_range = (SELECT MAX(RESTAURANTS.Price_range) -- Gets the
 												FROM TRAVEL_ATTRACTIONS
                                                 GROUP BY Owner_ID
                                                 ORDER BY count_owners DESC
-                                                ) AS ASSETS)
-LIMIT 2;
+                                                ) AS ASSETS) LIMIT 2;
 
+
+
+-- Eric Gutierrez's Queries
 -- Query7: Retrieve the names of the 5 most desirable France cities to visit
 SELECT 
     CITY.City_Name,  -- The name of the city from the CITY table
@@ -109,6 +87,7 @@ ORDER BY
     NumberOfTrips DESC  -- Order cities by the number of trip plans, descending
 LIMIT 5;  -- Limits results to the top 5 cities
 
+-- Eric Gutierrez's Queries
 -- Query 8. Retrieve the username and status of the member who either posted any comments or created any itinerary between 12/01/2018 and 1/31/2019.
 SELECT DISTINCT AM.User_Name, AM.Is_Preferred
 FROM AUTHORIZED_MEMBER AM
@@ -124,6 +103,9 @@ WHERE AM.Member_ID IN (
     WHERE C.Comment_Date BETWEEN '2018-12-01' AND '2019-01-31' -- Checking if the date of the comment is within the date range (Only one time since we only have one date)
 );
 
+
+
+ -- Emilio Zuniga's Queries:
 /* Query 4. Retrieve the names and countries of all 
  * preferred users who never visited the US. */
 SELECT AuM.FName, AuM.LName
@@ -137,6 +119,7 @@ INNER JOIN Country Co ON S.Country_Name = Co.Country_Name
 WHERE Co.Country_Name NOT LIKE "USA"
 GROUP BY AuM.Member_ID;
 
+ -- Emilio Zuniga's Queries:
 /* Query 9. For each member who has visited all the
  * states/provinces in France, retrieve the maximum, 
  * minimum and average daily cost per person, per day 
@@ -159,3 +142,4 @@ FROM(
 )AS subquery
 INNER JOIN AUTHORIZED_MEMBER AuM ON subquery.Member_ID = AuM.Member_ID
 GROUP BY AuM.FName, AuM.LName;
+
