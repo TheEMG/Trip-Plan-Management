c2011f511e9a9f9980c5f4060597f71e0897c3f0
Eric Oliver <erico49867@gmail.com>
2024-04-27 11:25:31 -0500

    Added in View 1

--- views.sql
+++ views.sql
@@ -1,5 +1,34 @@
 USE Erics;
 
+-- create view 1
+-- Retrieve all restaurants in San Antonio, Texas, USA
+CREATE VIEW Restaurants_San_Antonio_Texas_USA AS
+SELECT 
+    R.Restaurant_ID,
+    R.Restaurant_name,
+    R.Restaurant_address,
+    R.Rating
+FROM 
+    RESTAURANTS R
+JOIN 
+    TRAVEL_ATTRACTIONS TA ON R.Attraction_ID = TA.Attraction_ID
+JOIN
+    CITY C ON TA.City_ID = C.City_ID
+JOIN
+    STATE S ON C.State_ID = S.State_ID
+JOIN 
+    COUNTRY CTR ON S.Country_Name = CTR.Country_Name
+WHERE 
+    CTR.Country_Name = 'USA' 
+    AND S.State_name = 'Texas'
+    AND C.City_Name = 'San Antonio';
+    
+    SELECT *
+FROM Restaurants_San_Antonio_Texas_USA;
+
+
+
+-- View 2
 CREATE VIEW View_CountriesAndMembersVisited AS
 SELECT 
     COUNTRY.Country_Name,
